afb4a2a2c8458366e41053a372613d211dd45c43
Josef Utbult <josef@utbult.design>
2023-03-13 21:55:06 +0100

    Fixed boot problems

--- app.py
+++ app.py
@@ -12,31 +12,40 @@ DEBUG = False
 logging.basicConfig(filename=os.path.dirname(__file__) + '/XP_TV.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO if not DEBUG else logging.DEBUG)
 
 def setup():
-    chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[CHROMECAST_NAME])
+    while True:
+        logging.info(f"Trying to connect to {CHROMECAST_NAME}")
+        chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[CHROMECAST_NAME])
 
-    cast = None
-    try:
-        cast = chromecasts[0]
-        logging.info("Connected to chromecast")
-        cast.wait()
-        logging.debug(cast.status)
-        return True, cast
-    except IndexError:
-        logging.error(f"Unable to find {CHROMECAST_NAME}")
-        return False, None
+        try:
+            cast = chromecasts[0]
+            logging.info(f"Connected to {CHROMECAST_NAME}")
+            cast.wait()
+            logging.debug(cast.status)
+            return cast
+        except IndexError:
+            logging.error(f"Unable to find {CHROMECAST_NAME}")
+
+        sleep(5)
+
 
 def play(cast):
     url = URL + STREAM_NAME
     streamlink_url = None
-    try:
-        streamlink_url = streamlink.streams(url)['best'].url
-        logging.info(f"Got stream from URL {url}")
-    except KeyError:
-        logging.error(f"Unable to open stream from URL {url}")
+    while True:
+        logging.info(f"Connecting to {url}")
+        try:
+            streamlink_url = streamlink.streams(url)['best'].url
+            logging.info(f"Got stream from URL {url}")
+            cast.media_controller.play_media(streamlink_url, 'video/mp4')
+            cast.media_controller.block_until_active()
+            logging.debug(cast.media_controller.status)
 
-    cast.media_controller.play_media(streamlink_url, 'video/mp4')
-    cast.media_controller.block_until_active()
-    logging.debug(cast.media_controller.status)
+            return
+        except KeyError:
+            logging.error(f"Unable to open stream from URL {url}")
+        except OSError:
+            logging.error(f"Network is unreachable")
+
 
 def check_active(cast):
     logging.debug(cast.status)
@@ -45,12 +54,12 @@ def check_active(cast):
     except KeyError:
         return False
 
+
 if __name__=='__main__':
+    cast = setup()
+
     while True:
-        status, cast = setup()
-        if not status:
-            pass
-        elif not check_active(cast):
+        if not check_active(cast):
             logging.info("Reconnecting")
             play(cast)
 
